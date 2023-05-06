128090aceccfcf23aeee39448f10c7f438f31705
aleksejrevakin <alexey.amahasla@gmail.com>
2023-05-06 17:32:17 +0300

    Create project

---
+++ TicTacToe/Controllers/MainController.swift
@@ -0,0 +1,27 @@
+//
+//  MainController.swift
+//  TicTacToe
+//
+//  Created by Алексей Ревякин on 06.05.2023.
+//
+
+import UIKit
+
+final class MainController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configure()
+        layout()
+    }
+    
+    func configure() {
+        view.backgroundColor = .cyan
+    }
+    
+    func layout() {
+        
+    }
+
+}
+
