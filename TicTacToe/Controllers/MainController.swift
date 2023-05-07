b05173a29fefbe3aaaa5bc44b1600876bdf6f781
aleksejrevakin <alexey.amahasla@gmail.com>
2023-05-07 15:28:50 +0300

    Create symbol and func isWin

--- TicTacToe/Controllers/MainController.swift
+++ TicTacToe/Controllers/MainController.swift
@@ -9,6 +9,8 @@ import UIKit
 import SnapKit
 
 final class MainController: UIViewController {
+    private var grid: [String] = Array(repeating: ".", count: 9)
+    private var turn: Bool = true
     private lazy var collection: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.minimumLineSpacing = 3
@@ -17,7 +19,7 @@ final class MainController: UIViewController {
         let collection = UICollectionView(frame: .zero, collectionViewLayout: layout)
         collection.dataSource = self
         collection.delegate = self
-        collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cell")
+        collection.register(GridCell.self, forCellWithReuseIdentifier: "cell")
         collection.backgroundColor = .cyan
         return collection
     }()
@@ -49,15 +51,65 @@ extension MainController: UICollectionViewDataSource, UICollectionViewDelegate {
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! GridCell
         cell.backgroundColor = .white
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(indexPath.row)
+        guard isFree(num: indexPath.row) else {return}
+        let cell = collectionView.cellForItem(at: indexPath) as! GridCell
+        if turn {
+            cell.configure(with: .circle)
+            grid[indexPath.row] = "0"
+            turn = !turn
+            if isWin() {
+                print("Win")
+            }
+        } else {
+            cell.configure(with: .cross)
+            grid[indexPath.row] = "x"
+            turn = !turn
+            if isWin() {
+                print("Win")
+            }
+        }
     }
     
     
+}
+
+extension MainController {
+    func isFree(num: Int) -> Bool{
+        return grid[num] == "."
+    }
+    
+    func isWin() -> Bool{
+        if grid[0] == grid[1] && grid[1] == grid[2] && grid[2] != "." {
+            return true
+        }
+        if grid[0] == grid[4] && grid[4] == grid[2] && grid[2] != "." {
+            return true
+        }
+        if grid[0] == grid[3] && grid[3] == grid[6] && grid[6] != "." {
+            return true
+        }
+        if grid[1] == grid[4] && grid[4] == grid[7] && grid[7] != "." {
+            return true
+        }
+        if grid[2] == grid[4] && grid[4] == grid[6] && grid[6] != "." {
+            return true
+        }
+        if grid[2] == grid[5] && grid[5] == grid[8] && grid[8] != "." {
+            return true
+        }
+        if grid[3] == grid[4] && grid[4] == grid[5] && grid[5] != "." {
+            return true
+        }
+        if grid[6] == grid[7] && grid[7] == grid[8] && grid[8] != "." {
+            return true
+        }
+        return false
+    }
 }
 
---
+++ TicTacToe/Views/GridCell.swift
@@ -0,0 +1,52 @@
+//
+//  GirdCell.swift
+//  TicTacToe
+//
+//  Created by Алексей Ревякин on 07.05.2023.
+//
+
+import UIKit
+
+enum Symbol {
+    case cross
+    case circle
+}
+
+class GridCell: UICollectionViewCell {
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initialize()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        initialize()
+    }
+    
+    func initialize() {
+        contentView.addSubview(imageView)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        imageView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+    }
+    
+    func configure(with symbol: Symbol) {
+        switch symbol {
+        case .cross:
+            imageView.image = UIImage(systemName: "multiply")
+            imageView.tintColor = .red
+        case .circle:
+            imageView.image = UIImage(systemName: "circle")
+        }
+    }
+}
